c8e8291653b0ec2f6760689a43d2ed029fa80831
doquoctuananh <71720140+doquoctuananh@users.noreply.github.com>
2025-07-03 10:57:26 +0700

    form register

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,26 @@
+import styles from '~pages/register/Register.module.scss'
+import clsx from 'clsx';
+import MyInput from '../../components/Input/MyInput.jsx';
+import MyButton from '~components/Button/MyButton.jsx';
 function Register() {
     return ( 
-        <div>
-            Register Form
+        <div className={clsx(styles.register)}>
+            
+            <div className={clsx(styles.register_form)}>
+                <h2>Đăng kí</h2>
+                <form className={clsx(styles.form)}>
+                    <MyInput name = "Name"/>
+                    <MyInput name = "Email"/>
+                    <MyInput name = "Address"/>
+                    <MyInput name = "Phone"/>
+                    <MyInput name = "Gender"/>
+                    <MyInput name = "Password"/>
+                </form>
+                
+                <div className={clsx(styles.form_footer)}>
+                    <MyButton primary>Đăng kí</MyButton>
+                </div>
+            </div>
         </div>
      );
 }
