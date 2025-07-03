9a229c59d7312477799620d162f478d01fc71ad8
doquoctuananh <71720140+doquoctuananh@users.noreply.github.com>
2025-07-03 11:15:02 +0700

    form login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,8 +1,21 @@
 import styles from '~pages/login/Login.module.scss'
-
+import clsx from 'clsx';
+import MyInput from '../../components/Input/MyInput';
+import MyButton from '../../components/Button/MyButton';
 function Login() {
     return ( 
-        <h1>login</h1>
+        <div className={clsx(styles.login)}>
+            <div className={clsx(styles.form_login)}> 
+                <h2>Đăng Nhập</h2>
+                <form className={clsx(styles.form)}>
+                    <MyInput  name = "UserName"/>
+                    <MyInput  name = "Password"/>
+                </form>
+                <div className={clsx(styles.form_footer)}>
+                    <MyButton to="/" link primary>Đăng Nhập</MyButton>
+                </div>
+            </div>
+        </div>
     );
 }
 
