37f6d25d3cfdff17170ecb2b8eee39c5e82bfa70
doquoctuananh <71720140+doquoctuananh@users.noreply.github.com>
2025-07-03 10:04:16 +0700

    components Layouts Header

--- src/components/Layouts/Header/Header.jsx
+++ src/components/Layouts/Header/Header.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 import  styles from '../Header/Header.module.scss'
 import clsx from 'clsx';
-import assets from '~/assets/index.jsx'
+import assets from '~/assets/index.jsx';
+
+import MyButton from '../../Button/MyButton.jsx';
+
 function Header() {
+
+    const [search,setSearch] = useState("")
+
     return ( <div className= {clsx(styles.heading)}>
         
         <div className={clsx(styles.heading_content)}>
@@ -10,23 +18,36 @@ function Header() {
             </div>
             <div className={clsx(styles.content_category)}>
                 <ul className={clsx(styles.category_list)}>
-                    <li className={clsx(styles.category_item)}>Trang chu</li>
-                    <li className={clsx(styles.category_item)}>Gioi thieu</li>
-                    <li className={clsx(styles.category_item)}>Tin tuc</li>
-                    <li className={clsx(styles.category_item)}>Lien he</li>
+                    <Link to = "/" className={clsx(styles.category_item)}>Trang chu</Link>
+                    <Link to="/infor" className={clsx(styles.category_item)}>Gioi thieu</Link>
+                    <Link to="/news" className={clsx(styles.category_item)}>Tin tuc</Link>
+                    <Link to="/contact" className={clsx(styles.category_item)}>Lien he</Link>
                 </ul>
             </div>
 
             <div className={clsx(styles.content_search)}>
-
+                <input 
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    placeholder='Search' 
+                />
+                <MyButton 
+                    onClick = {(e) => {
+                    console.log(search)
+                    setSearch('')   
+                    
+                }
+                }
+                >
+                    Search
+                </MyButton>
             </div>
 
             <div className={clsx(styles.content_footer)}>
-                <button>Đăng kí</button>
-                <button>Đăng nhập</button>
+                <MyButton primary link to="/register">Đăng kí</MyButton>
+                <MyButton primary link to="/login">Đăng nhập</MyButton>
             </div>
             
-
         </div>
     </div> );
 }
