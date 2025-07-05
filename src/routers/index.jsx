5c6bf164914961108f945aa469ea78bbca9f35f2
doquoctuananh <71720140+doquoctuananh@users.noreply.github.com>
2025-07-05 11:14:06 +0700

    path and component admin

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,16 +1,45 @@
 import Home from '~pages/home/Home.jsx'
-import Cart from '~pages/cart/Cart.jsx'
-import BookDetail from '~pages/bookdetail/BookDetail.jsx'
-import Payment from '~pages/payment/Payment.jsx'
+import News from '~pages/news/News.jsx'
+import Infor from '~pages/infor/Infor.jsx'
+import Contact from '~pages/contact/Contact.jsx'
+import Login from '~pages/login/Login.jsx'
+import Register from '~pages/register/Register'
+
+//user
+import Book from "~pages/book/Book.jsx"
+
+//admin
+import HomeDashboard from '~dashboard/pagesdashboard/home/HomeDashboard.jsx'
+import BookDashboard from '~dashboard/pagesdashboard/books/BookDashboard.jsx'
+import OrderDashboard from '~dashboard/pagesdashboard/orders/OrderDashboard.jsx'
+import TypeBookDashboard from '~dashboard/pagesdashboard/type_books/TypeBookDashboard.jsx'
+import ImportBookDashboard from '~dashboard/pagesdashboard/import_books/ImportBookDashboard.jsx'
+import UserDashboard from '~dashboard/pagesdashboard/users/UserDashboard.jsx'
 
 const publicRoute = [
     {path: "/",component: Home},
-    {path: "/book/cart",component: Cart},
-    {path: "/book/bookdetail",component: BookDetail},
-    {path: "/book/payment",component: Payment}
+    {path: "/news",component: News},
+    {path: "/infor",component: Infor},
+    {path: "/contact",component: Contact},
+    {path: "/login",component: Login},
+    {path: "/register",component: Register}
     
 ]
 
-const privateRoute = []
+// user 
+const privateRoute = [
+    {path:"/api/books",component : Book}
+]
 
-export {publicRoute,privateRoute}
+//admin
+
+const privateRouteDashboard = [
+    {path : "/api/dashboard", component : HomeDashboard},
+    {path : "/api/dashboard/books", component : BookDashboard},
+    {path : "/api/dashboard/orders", component : OrderDashboard},
+    {path : "/api/dashboard/typebooks", component : TypeBookDashboard},
+    {path : "/api/dashboard/importbooks", component : ImportBookDashboard},
+    {path : "/api/dashboard/users", component : UserDashboard}
+]
+
+export {publicRoute,privateRoute,privateRouteDashboard}
